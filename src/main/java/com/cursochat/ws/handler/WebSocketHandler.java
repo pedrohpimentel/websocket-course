bc0ca7acb769ac75f8ec4940720e4b6183afb295
Pedro Pimentel <pedrohenriquepimenteldev05@gmail.com>
2025-12-06 07:27:49 -0300

    feat: Recebendo mensagens do backend

--- src/main/java/com/cursochat/ws/handler/WebSocketHandler.java
+++ src/main/java/com/cursochat/ws/handler/WebSocketHandler.java
@@ -6,6 +6,12 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+
 /*
 * A classe WebSocketHandler foi criada para gerenciar o ciclo de vida e a interação com as conexões WebSocket.
 * Ele irá atuar como um ponto de entrada para as minhas mensagens WebSocket,
@@ -22,6 +28,20 @@ public class WebSocketHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
         System.out.println("afterConnectionEstablished session id : " + session.getId());
+
+        /* Fazendo testes de loops para mostrar o envio de eventos para o front-end
+        */
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    session.sendMessage(new TextMessage("Olá " + UUID.randomUUID()));
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        },2000L,2000L);
     }
 
     /*É chamado toda vez que o servidor receber uma mensagem de texto de um cliente conectado*/
